58ea333d95df6afb7b1027fee16f9aabb72b5c29
ndaboom <ndabosam084@gmail.com>
2022-10-10 18:00:09 +0200

    Linters errors fixed

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,18 @@
-const toggleMenu = document.querySelector(".toggleNav");
-const popup = document.querySelector(".popup");
-const close = document.querySelector(".close_icon");
-const menu_options = document.querySelectorAll(".popup_menu_option");
+const toggleMenu = document.querySelector('.toggleNav');
+const popup = document.querySelector('.popup');
+const close = document.querySelector('.close_icon');
+const menuOptons = document.querySelectorAll('.popup_menu_option');
 
-toggleMenu.addEventListener("click", () => {
-  popup.classList.remove("hide");
+toggleMenu.addEventListener('click', () => {
+  popup.classList.remove('hide');
 });
 
-close.addEventListener("click", () => {
-  popup.classList.add("hide");
+close.addEventListener('click', () => {
+  popup.classList.add('hide');
 });
 
-menu_options.forEach((option) => {
-  option.addEventListener("click", () => {
-    popup.classList.add("hide");
+menuOptons.forEach((option) => {
+  option.addEventListener('click', () => {
+    popup.classList.add('hide');
   });
 });
