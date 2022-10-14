c63406344d102c3df498d476863d040b48505136
ndaboom <ndabosam084@gmail.com>
2022-10-14 23:05:19 +0200

    Eslint issue refixed

--- assets/main.js
+++ assets/main.js
@@ -39,7 +39,7 @@ const title = document.querySelector('.modal_title');
 const image = document.querySelector('.project_illustration');
 const description = document.querySelector('.project_description');
 const tech = document.querySelector('.technologies');
-const modal_close_btn = document.querySelector('.close-button');
+const modalCloseBtn = document.querySelector('.close-button');
 const workSection = document.querySelector('.works_list');
 
 // Modal menu toggle
@@ -47,7 +47,7 @@ toggleMenu.addEventListener('click', () => {
   popup.classList.remove('hide');
 });
 
-//Close mobile menu
+// Close mobile menu
 close.addEventListener('click', () => {
   popup.classList.add('hide');
 });
@@ -59,55 +59,49 @@ menuOptons.forEach((option) => {
   });
 });
 
-/** Modal close event  */ 
-modal_close_btn.addEventListener('click', () => {
-    modalSection.classList.add('hide');
-    tech.innerHTML = ` `;
+// Modal close event
+modalCloseBtn.addEventListener('click', () => {
+  modalSection.classList.add('hide');
+  tech.innerHTML = ' ';
 });
 
-modalSection.addEventListener('click', () =>{
+modalSection.addEventListener('click', () => {
   modalSection.classList.add('hide');
-  tech.innerHTML = ` `;
+  tech.innerHTML = ' ';
 });
 
 // works array
 const works = [
-  {
-  name: "Keeping track of hundreds of components",
-  img_src: "./assets/images/project-img.png",
-  details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release",
+  {  name: 'Keeping track of hundreds of components',
+  img_src: './assets/images/project-img.png',
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
   technologies: ['Ruby on rails', 'Css', 'Javascript', 'Html']
 },
-{
-  name: "Keeping track of hundreds of components",
-  img_src: "./assets/images/project-img.png",
-  details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release",
+{  name: 'Keeping track of hundreds of components',
+  img_src: './assets/images/project-img.png',
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
   technologies: ['Ruby on rails', 'Css', 'Javascript', 'Html']
 },
-{
-  name: "Keeping track of hundreds of components",
-  img_src: "./assets/images/project-img.png",
-  details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+{  name: 'Keeping track of hundreds of components',
+  img_src: './assets/images/project-img.png',
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
   technologies: ['Ruby on rails', 'Css', 'Javascript', 'Html']
 },
-{
-  name: "Keeping track of hundreds of components",
-  img_src: "./assets/images/project-img.png",
-  details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+{  name: 'Keeping track of hundreds of components',
+  img_src: './assets/images/project-img.png',
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
   technologies: ['Ruby on rails', 'Css', 'Javascript', 'Html']
 },
-{
-  name: "Keeping track of hundreds of components",
-  img_src: "./assets/images/project-img.png",
-  details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+{  name: 'Keeping track of hundreds of components',
+  img_src: './assets/images/project-img.png',
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
   technologies: ['Ruby on rails', 'Css', 'Javascript', 'Html']
 },
-{
-  name: "Keeping track of hundreds of components",
-  img_src: "./assets/images/project-img.png",
-  details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release.",
+{  name: 'Keeping track of hundreds of components',
+  img_src: './assets/images/project-img.png',
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
   technologies: ['Ruby on rails', 'Css', 'Javascript', 'Html']
-}]
+}];
 
 // Populate modal ui function
 function populateUI(project) {
@@ -115,9 +109,8 @@ function populateUI(project) {
   image.src = project.img_src;
   description.innerHTML = project.details;
   project.technologies.forEach((item) => {
-        tech.innerHTML += `<li>${item}</li>`
-  })
-
+  tech.innerHTML += `<li>${item}</li>`;
+  });
   modalSection.classList.remove('hide');
 }
 
@@ -125,7 +118,6 @@ function populateUI(project) {
 function createCard(worksItem) {
   const workContainer = document.createElement('div');
   workContainer.className = 'card';
-
   const worksImg = document.createElement('img');
   worksImg.className = 'card-img';
   worksImg.setAttribute('src', 'assets/images/img-placeholder.png');
@@ -147,7 +139,7 @@ function createCard(worksItem) {
   }
   cardTagsContainer.innerHTML = liOne;
   cardFooter.appendChild(cardTagsContainer);
-
+  
   const action = document.createElement('div');
   action.className = 'action';
 
@@ -155,18 +147,14 @@ function createCard(worksItem) {
   cardButton.classList = 'card-btn btn-primary primary-font-style modalToggle';
   cardButton.innerHTML = 'See Project';
   action.appendChild(cardButton);
-
   cardFooter.appendChild(action);
-
   workContainer.appendChild(cardFooter);
-
   workSection.appendChild(workContainer);
 }
 
-
 const populateWorks = (arr) => {
   for (let i = 0; i < arr.length; i += 1) {
-    createCard(arr[i]);
+  createCard(arr[i]);
   }
 };
 
