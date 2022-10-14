6cb9efc4901bdb8ce3cc8660d79a15444f193b37
ndaboom <ndabosam084@gmail.com>
2022-10-15 00:03:54 +0200

    ESlint bug fixed

--- assets/form-validation.js
+++ assets/form-validation.js
@@ -1,38 +1,38 @@
 /** Mobile & Desktop form validation */
-const mobile_form = document.querySelector('#mobile_form');
+const mobileForm = document.querySelector('#mobile_form');
 const mEmail = document.getElementById('mEmail');
 const validationM = document.getElementById('memail_validation');
 
-const desktop_form = document.querySelector('#desktop_form');
+const desktopForm = document.querySelector('#desktop_form');
 const dEmail = document.getElementById('dEmail');
 const validationD = document.getElementById('demail_validation');
 
-const EMAIL_INVALID = "Please enter a correct email address format";
+const EMAIL_INVALID = 'Please enter a correct email address format';
 
-function validateForm(){
-    if (mEmail.value === mEmail.value.toLowerCase()) {
-    mobile_form.submit();
-    mobile_form.reset();
-    } else {
+function validateForm() {
+  if (mEmail.value === mEmail.value.toLowerCase()) {
+    mobileForm.submit();
+    mobileForm.reset();
+  } else {
     validationM.innerHTML = EMAIL_INVALID;
-    }
+  }
 }
 
-function desktopValidateForm(){
-    if (dEmail.value === dEmail.value.toLowerCase()) {
-    desktop_form.submit();
-    desktop_form.reset();
-    } else {
+function desktopValidateForm() {
+  if (dEmail.value === dEmail.value.toLowerCase()) {
+    desktopForm.submit();
+    desktopForm.reset();
+  } else {
     validationD.innerHTML = EMAIL_INVALID;
-    }
+  }
 }
 
-mobile_form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    validateForm();
+mobileForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validateForm();
 });
 
-desktop_form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    desktopValidateForm();
+desktopForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  desktopValidateForm();
 });
